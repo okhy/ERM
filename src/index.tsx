cb14ed727c7137d1a5f54638199d862e026ac586
okhy <dawid_sokol@epam.com>
2019-03-25 14:21:05 +0100

    Add button to test imports

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 // components
 import WithError from "./components/WithError/WithError";
+import Button from "./components/Button/Button";
 
 ReactDOM.render(
   <div className="erm-main-wrapper">
     <WithError>
       <h1>Hello React</h1>
+      <Button label="test button" />
     </WithError>
   </div>,
   document.getElementById("app")
