c5d5220e4a9c2eb908c8eb5f5595914eb224cf39
okhy <dawid_sokol@epam.com>
2019-03-25 15:55:02 +0100

    Develop MoviesGrid
    add conditional rendering

--- src/components/MovieGrid/MovieGrid.tsx
+++ src/components/MovieGrid/MovieGrid.tsx
@@ -1,26 +1,44 @@
 import * as React from "react";
 // components
 import MovieGridItem from "./components/MovieGridItem/MovieGridItem";
+// interface
+interface IMoviesGird {
+  similarResults: false;
+  movies?: any[];
+}
 
-const MovieGrid = () => (
+const MovieGrid = (props: IMoviesGird) => (
   <div className="main">
-    <div className="results">
-      <span className="count">7 movies found</span>
-      <div className="sorting">
-        <span>Sort by</span>
-        <button>release date</button>
-        <button>rating</button>
+    {props.similarResults ? (
+      <div>
+        <span>Films by </span>
+        <span>Drama genre</span>
       </div>
-    </div>
-    <div className="grid">
-      <MovieGridItem />
-      <MovieGridItem />
-      <MovieGridItem />
-      <MovieGridItem />
-      <MovieGridItem />
-      <MovieGridItem />
-      <MovieGridItem />
-    </div>
+    ) : (
+      <div className="results">
+        <span className="count">7 movies found</span>
+        <div className="sorting">
+          <span>Sort by</span>
+          <button>release date</button>
+          <button>rating</button>
+        </div>
+      </div>
+    )}
+    {props.movies ? (
+      <div className="grid">
+        <MovieGridItem />
+        <MovieGridItem />
+        <MovieGridItem />
+        <MovieGridItem />
+        <MovieGridItem />
+        <MovieGridItem />
+        <MovieGridItem />
+      </div>
+    ) : (
+      <div>
+        <span>No movies :(</span>
+      </div>
+    )}
   </div>
 );
 
