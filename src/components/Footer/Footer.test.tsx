01bac677b6aae6e86c2efa91d3d9a41544530fa4
okhy <dawid_sokol@epam.com>
2019-03-25 15:17:45 +0100

    Add tests and update test script

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import { shallow } from "enzyme";
 // components
 import Footer from "./Footer";
+import LogoBar from "./../LogoBar/LogoBar";
 
 describe("Footer component...", () => {
   it("...renders without errors", () => {
     const wrapper = shallow(<Footer />);
 
     expect(wrapper.find(Footer)).toBeTruthy();
+    expect(wrapper.find(LogoBar)).toBeTruthy();
   });
 });
--- src/views/SearchPage/SearchPage.test.tsx
+++ src/views/SearchPage/SearchPage.test.tsx
@@ -2,11 +2,15 @@ import * as React from "react";
 import { shallow } from "enzyme";
 // components
 import SearchPage from "./SearchPage";
+import Header from "./../../components/Header/Header";
+import Footer from "./../../components/Footer/Footer";
 
 describe("SearchPage component...", () => {
   it("...renders without errors", () => {
     const wrapper = shallow(<SearchPage />);
 
     expect(wrapper.find(SearchPage)).toBeTruthy();
+    expect(wrapper.find(Header)).toBeTruthy();
+    expect(wrapper.find(Footer)).toBeTruthy();
   });
 });
