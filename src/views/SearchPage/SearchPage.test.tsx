76da7d3791ad57f72c90af053fc24c47edd361b0
okhy <dawid_sokol@epam.com>
2019-03-26 16:18:25 +0100

    Add basic e2e test using puppeteer

--- src/views/SearchPage/SearchPage.test.tsx
+++ src/views/SearchPage/SearchPage.test.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
+import * as puppeteer from "puppeteer";
 import { shallow } from "enzyme";
+const path = require("path");
+
 // components
 import SearchPage from "./SearchPage";
 import Header from "./../../components/Header/Header";
@@ -16,5 +19,20 @@ describe("SearchPage component...", () => {
   it("... matches snapshot", () => {
     const wrapper = shallow(<SearchPage />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  // INTERGRATION TESTS
+  describe("... passes integration e2e tests ...", () => {
+    it("... accepts input", async () => {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+
+      await page.goto(path.resolve(__dirname, "./../../../dist/index.html"));
+      await page.type("input", "test movie title");
+      await page.click("button");
+      const text = await page.$eval("input", el => el.getAttribute("value"));
+
+      expect(text).toBe("test movie title");
+    });
   });
 });
