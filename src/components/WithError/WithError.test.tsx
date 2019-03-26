2092c1458a34ebdbfeceb685d24f4792a46e6b9a
okhy <dawid_sokol@epam.com>
2019-03-26 14:54:06 +0100

    Expand WithError test to 100% coverage

--- src/components/WithError/WithError.test.tsx
+++ src/components/WithError/WithError.test.tsx
@@ -7,14 +7,14 @@ describe("WithError component...", () => {
   it("... renders without errors", () => {
     const wrapper = shallow(<WithError />);
 
-    expect(wrapper.state("hasError")).toBeFalsy();
+    expect(wrapper.state("hasError")).toEqual(false);
     expect(wrapper.find(WithError)).toBeTruthy();
   });
-  it('... matches snapshot', () => {
+  it("... matches snapshot", () => {
     const wrapper = shallow(<WithError />);
     expect(wrapper).toMatchSnapshot();
-  })
-  it("...renders children", () => {
+  });
+  it("... renders children", () => {
     const TestComponent = () => <span>Test</span>;
     const wrapper = shallow(
       <WithError>
@@ -24,6 +24,7 @@ describe("WithError component...", () => {
 
     expect(wrapper.find(TestComponent)).toBeTruthy();
   });
+  // error related tests
   describe("... handles errors by ...", () => {
     it("... calling componentDidCatch", () => {
       const TestComponent = () => <span>Test</span>;
@@ -33,15 +34,29 @@ describe("WithError component...", () => {
         </WithError>
       );
 
-      // mocking as jest function
-      wrapper.instance().componentDidCatch = jest.fn();
+      // wrapping method as jest function for spying on
+      const componentDidCatch = wrapper.instance().componentDidCatch;
+      wrapper.instance().componentDidCatch = jest.fn(componentDidCatch);
       wrapper.simulateError(new Error());
 
       expect(wrapper.instance().componentDidCatch).toHaveBeenCalled();
     });
-    it("... returning correct state by getDerivedStateFromError", () => {
+    it("... returning correct state using getDerivedStateFromError", () => {
       const result = WithError.getDerivedStateFromError();
       expect(result.hasError).toBe(true);
+    });
+    it("... rendering error message", () => {
+      const TestComponent = () => <span>Test</span>;
+      const wrapper = shallow(
+        <WithError>
+          <TestComponent />
+        </WithError>
+      );
+
+      wrapper.setState({ hasError: true });
+
+      expect(wrapper.state("hasError")).toEqual(true);
+      expect(wrapper.contains(<span>Something went wrong</span>)).toBeTruthy();
     });
   });
 });
