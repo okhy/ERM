7af88a1b66baa9726018e1c7b4a4eccdc15e0dab
okhy <dawid_sokol@epam.com>
2019-03-28 12:14:48 +0100

    Style disabled button state + add it to storybook.

--- src/components/Button/Button.story.tsx
+++ src/components/Button/Button.story.tsx
@@ -5,4 +5,5 @@ import Button from "./Button";
 storiesOf("Components / Button", module)
   .add("default", () => <Button label="default" />)
   .add("primary", () => <Button label="primary" type="primary" />)
-  .add("negative", () => <Button label="negative" type="negative" />);
+  .add("negative", () => <Button label="negative" type="negative" />)
+  .add("disabled", () => <Button label="disabled" type="disabled" disabled={true} />);
