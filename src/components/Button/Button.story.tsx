d13ec91f34dadf7a90cf8b5d2330541a27b5883a
okhy <dawid_sokol@epam.com>
2019-03-29 12:56:20 +0100

    Add small variant to Button component

--- src/components/Button/Button.story.tsx
+++ src/components/Button/Button.story.tsx
@@ -3,16 +3,35 @@ import { storiesOf } from "@storybook/react";
 import Button from "./Button";
 
 storiesOf("Components / Button", module)
-  .add("basic", () => <Button label="basic" />)
-  .add("primary", () => <Button label="primary" type="primary" />)
-  .add("negative", () => <Button label="negative" type="negative" />)
-  .add("disabled", () => <Button label="disabled" type="disabled" disabled={true} />)
-  .add("with icon", () => <Button label="basic" icon="fab fa-font-awesome-flag" />)
+  .add("default variants", () => (
+    <>
+      <Button label="basic" />
+      <Button label="primary" type="primary" />
+      <Button label="negative" type="negative" />
+      <Button label="disabled" type="disabled" disabled={true} />
+    </>
+  ))
+  .add("with icon", () => (
+    <>
+      <Button label="basic" icon="fab fa-font-awesome-flag" />
+      <Button label="primary" type="primary" icon="fab fa-font-awesome-flag" />
+      <Button label="negative" type="negative" icon="fab fa-font-awesome-flag" />
+      <Button label="disabled" type="disabled" disabled={true} icon="fab fa-font-awesome-flag" />
+    </>
+  ))
   .add("small variants", () => (
     <>
       <Button label="small" size="small" />
       <Button label="small-primary" type="primary" size="small" />
       <Button label="small-negative" type="negative" size="small" />
       <Button label="small-disabled" type="disabled" size="small" />
+    </>
+  ))
+  .add("small variants with icons", () => (
+    <>
+      <Button label="small" size="small" icon="fab fa-font-awesome-flag" />
+      <Button label="small-primary" type="primary" size="small" icon="fab fa-font-awesome-flag" />
+      <Button label="small-negative" type="negative" size="small" icon="fab fa-font-awesome-flag" />
+      <Button label="small-disabled" type="disabled" size="small" icon="fab fa-font-awesome-flag" />
     </>
   ));
