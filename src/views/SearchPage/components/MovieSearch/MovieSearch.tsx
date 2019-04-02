87f991d5e51df87a9884b6a31ee15e8319ba0b7e
okhy <dawid_sokol@epam.com>
2019-04-02 14:11:06 +0200

    Migrate MovieSearch to form

--- src/views/SearchPage/components/MovieSearch/MovieSearch.tsx
+++ src/views/SearchPage/components/MovieSearch/MovieSearch.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 // components
 import Button from "Components/Button/Button";
+// import Input from "../Input/Input";
 // styles
 import * as styles from "./MovieSearch.styles.css";
 
@@ -9,27 +10,35 @@ class MovieSearch extends React.Component<{}, { value: string }> {
     super(props);
     this.state = { value: "" };
   }
-  handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+  handleChange(event: React.ChangeEvent<HTMLInputElement>): void {
     this.setState({ value: event.target.value });
+  }
+  handleSubmit(event: any): void {
+    event.preventDefault();
   }
   render() {
     return (
-      <div className={styles.main}>
-        <span className={styles.searchLabel}>Find your movie</span>
-        <input
-          className={styles.searchField}
-          type="text"
-          placeholder="Movie title"
-          onChange={this.handleChange.bind(this)}
-          value={this.state.value}
-        />
+      <form className={styles.main} onSubmit={this.handleSubmit}>
+        <div className={styles.searchWrap}>
+          <div className={styles.searchField}>
+            <span className={styles.searchLabel}>Find a movie</span>
+            <input
+              className={styles.searchInput}
+              type="text"
+              placeholder={"Movie title"}
+              onChange={this.handleChange.bind(this)}
+              value={this.state.value}
+            />
+          </div>
+          {/* <Button label="search" type="primary" /> */}
+          <button type="submit">submit</button>
+        </div>
         <div className={styles.options}>
           <span className={styles.optionsLabel}>search by</span>
           <Button label="title" size="small" type="white" />
           <Button label="genre" size="small" type="white" />
         </div>
-        <Button label="search" type="primary" />
-      </div>
+      </form>
     );
   }
 }
