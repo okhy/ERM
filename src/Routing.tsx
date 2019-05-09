0c31a10c5ef997a5a084eb4063b1581f74f4a3d4
Dawid Sokół <david@okhy.pl>
2019-05-09 21:51:33 +0200

    Add ErrorPage to routing

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -4,12 +4,14 @@ import { HashRouter, Route, Switch } from "react-router-dom";
 // views
 import SearchPage from "Views/SearchPage/SearchPage";
 import DetailsPage from "Views/DetailsPage/DetailsPage";
+import ErrorPage from "Views/ErrorPage/ErrorPage";
 
 const Routes: React.SFC = () => (
   <HashRouter>
     <Switch>
       <Route exact path="/" component={SearchPage} />
       <Route exact path="/movie/:id" component={DetailsPage} />
+      <Route exact path="/404" component={ErrorPage} />
     </Switch>
   </HashRouter>
 );
