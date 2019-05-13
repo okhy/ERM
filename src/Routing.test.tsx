2d7247f3d5c0dae58fd508e266ddddd9cb17d6ea
okhy <dawid_sokol@epam.com>
2019-05-13 14:35:14 +0200

    Add Routing test stub

---
+++ src/Routing.test.tsx
@@ -0,0 +1,37 @@
+// import * as React from "react";
+// import { shallow } from "enzyme";
+// // router
+// import { MemoryRouter } from "react-router-dom";
+// import Routes from "./Routing";
+// // views
+// import SearchPage from "Views/SearchPage/SearchPage";
+// import DetailsPage from "Views/DetailsPage/DetailsPage";
+// import ErrorPage from "Views/ErrorPage/ErrorPage";
+
+// jest.mock("react-router-dom", () => ({
+//   Link: (props: any) => <div>{props.children}</div>,
+//   Route: (props: any) => <div>{props.children}</div>,
+//   Switch: (props: any) => <div>{props.children}</div>
+// }));
+
+// const getWrapperWithRoute = (route: string) =>
+//   shallow(
+//     <MemoryRouter initialEntries={[route]}>
+//       <Routes />
+//     </MemoryRouter>
+//   );
+
+// describe("Routing ... ", () => {
+//   it("... returns SearchPage", () => {
+//     const wrapper = getWrapperWithRoute("/");
+//     expect(wrapper.find(SearchPage)).toBeTruthy();
+//   });
+//   it("... returns DetailsPage", () => {
+//     const wrapper = getWrapperWithRoute("/movie/680");
+//     expect(wrapper.find(DetailsPage)).toBeTruthy();
+//   });
+//   it("... returns ErrorPage", () => {
+//     const wrapper = getWrapperWithRoute("404");
+//     expect(wrapper.find(ErrorPage)).toBeTruthy();
+//   });
+// });
