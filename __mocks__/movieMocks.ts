74cdec10a640094838aa80315a097abb33565afe
okhy <dawid_sokol@epam.com>
2019-05-14 11:12:54 +0200

    Fix mockdata to pass tests req

--- __mocks__/movieMocks.ts
+++ __mocks__/movieMocks.ts
@@ -30,13 +30,14 @@ export const mockMovieList: MovieTypes.IMovie[] = [
 
 export const mockResponseMovie: ResponseMovie = {
   id: 1,
-  vote_average: 4,
-  title: "Movie in array",
-  poster_path: "test poster path",
-  release_date: "1923",
-  genres: ["genre1", "genre2"],
-  runtime: 129,
-  overview: "overview"
+  vote_average: 8,
+  title: "Pulp Fiction",
+  poster_path: "https://image.tmdb.org/t/p/w500/dM2w364MScsjFf8pfMbaWUcWrR.jpg",
+  release_date: "1994-09-10",
+  genres: ["Thriller", "Crime"],
+  runtime: 154,
+  overview:
+    "A burger- loving hit man, his philosophical partner, a drug-addled gangster's moll and a washed-up boxer converge in this sprawling, comedic crime caper. Their adventures unfurl in three stories that ingeniously trip back and forth in time."
 };
 
 export const mockResponseMovieList: ResponseMovie[] = [
